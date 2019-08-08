9c0315fe5acd57daca5032b4856308fea5fad192
Live session user <a1705111@student.adelaide.edu.au>
2019-08-08 13:37:32 +0930

    Adds generic localSearch which uses operator interface objects

---
+++ LocalSearch.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class LocalSearch
+{
+	public static ArrayList<Double> search(TSP_Problem problem, ILocalSearchOperator operator)
+	{
+		ArrayList<Integer> current = problem.initPermutation();
+        ArrayList<Integer> next = new ArrayList<Integer>();
+        ArrayList<Integer> nextBest = new ArrayList<Integer>();
+
+        Double currentDistance = problem.getTotalDistance(current);
+        Double nextDistance;
+
+        ArrayList<Double> distances = new ArrayList<Double>();
+        distances.add(currentDistance);
+
+        boolean optimal = false;
+        while (!optimal)
+        {
+            nextDistance = currentDistance;
+            for (int i = 0; i < problem.getDimension(); i++)
+            {
+                for (int j = i+1; j < problem.getDimension(); j++)
+                {
+                    next = new ArrayList<Integer>(current);
+                    next = operator.mutate(next, i, j);
+                    if (nextDistance > problem.getTotalDistance(next))
+                    {
+                        nextBest = new ArrayList<Integer>(next);
+                        nextDistance = problem.getTotalDistance(next);
+                    }
+                }
+            }
+            if (nextDistance == currentDistance)
+            {
+                optimal = true;
+            }
+            else
+            {
+                current = new ArrayList<Integer>(nextBest);
+                currentDistance = nextDistance;
+                distances.add(currentDistance);
+            }
+        }
+        return distances;
+	}
+}
