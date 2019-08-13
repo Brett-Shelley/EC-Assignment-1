03bed327fb1bb8f04462710f52c3f4f9a00458ce
Live session user <a1705111@student.adelaide.edu.au>
2019-08-13 16:35:26 +0930

    Adds class for performing invertMutation

---
+++ InvertMutation.java
@@ -0,0 +1,66 @@
+import java.util.Random;
+
+public class InvertMutation implements IMutation
+{
+	public Solution mutate(Solution permutation)
+	{
+		// Copy permutation to avoid changing the orignal. 
+		Solution mutated = new Solution(permutation.getPermutation());
+		
+		int length = mutated.size();
+		Random rand = RandomNumberGenerator.getRandom();
+		int start = rand.nextInt(length);
+		int end = rand.nextInt(length);
+		int temp;
+		int current;
+		
+		// If start is before end, swap start and end. 
+		if (start > end)
+		{
+			temp = start;
+			start = end;
+			end = temp;
+		}
+		
+		// If start and end generate to have no intermediate
+		// cities, return without doing anything. 
+		// This avoids an infinite loop. 
+		if (start + 1 == end)
+		{
+			return mutated;
+		}
+		
+		// Perform the mutation. 
+		while(true)
+		{
+			start = start + 1;
+			end = end - 1;
+
+			// If swap would do nothing, we are done
+			// so break. 
+			// This is escape condition for case
+			// where start - end is an odd number,
+			// and hence the number in the middle is
+			// not changed. 
+			if (start == end)
+			{
+				break;
+			}
+			
+			mutated.swap(start, end);
+			
+			// If start and end are next to each other,
+			// we are done so break.
+			// This is the escape condition for the case
+			// when start - end is an even number, and hence
+			// the swapping stops when start and end are adjacent.  
+			if (start + 1 == end)
+			{
+				break;
+			}
+		}
+		
+		return mutated;
+	}
+}
+
