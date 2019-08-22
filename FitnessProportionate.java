c5f3248e7825cb8f7a4c46b96a6d6a9bc1cc05d0
Patates <38754583+pellway@users.noreply.github.com>
2019-08-22 16:45:28 +0930

    Updated fitProp, uses inverse to evaluate fitness

--- FitnessProportionate.java
+++ FitnessProportionate.java
@@ -3,35 +3,32 @@ import java.util.Random;
 
 public class FitnessProportionate implements ISelection
 {
-    Population populationObject;
-    
     // Keeps the solutions with higher fitness score with a higher probability
     // numSurvivors variable is number of solutions in population
     public Population select(TSP_Problem tsp, Population solutions, int numSurvivors)
     {
-        populationObject = new Population(tsp.getCoords(), tsp.getCoords().size());
-
         // Initialise population array
-        ArrayList<Solution> population = populationObject.getParents();
+        ArrayList<Solution> population = solutions.getParents();
 
         // Find sum of fitness
         double sum = 0;
         for (int i = 0; i < population.size(); i++)
         {
-            sum += population.get(i).getScore();
+            sum += 1/population.get(i).getScore();
         }
 
         // Select n individuals with probability assigned
-        double probability = sum;
+        double probability;
         double randomProb;
         ArrayList<Solution> survivors = new ArrayList<Solution> ();
         for (int i = 0; i < numSurvivors; i++)
         {
+            probability = 0;
             Random rand = RandomNumberGenerator.getRandom();
             randomProb = rand.nextDouble() * sum;
             for (int j = 0; j < population.size(); j++)
             {
-                if (probability < randomProb)
+                if (probability > randomProb)
                 {
                     survivors.add(population.get(j));
                     population.remove(j);
@@ -39,11 +36,11 @@ public class FitnessProportionate implements ISelection
                 }
                 else
                 {
-                    probability -= population.get(population.size() - j - 1).getScore();
+                    probability += 1/population.get(population.size() - j - 1).getScore();
                 }
             }
         }
-        populationObject.replaceParents(survivors);
-        return populationObject;
+        solutions.replaceParents(survivors);
+        return solutions;
     }
 }
